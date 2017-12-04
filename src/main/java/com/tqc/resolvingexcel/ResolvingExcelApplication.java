2ed7b3051640ce9f6a07709561ffa27975048a49
TQCCC <tqcccc@sina.com>
2017-12-04 23:04:36 +0800

    initial commit

---
+++ src/main/java/com/tqc/resolvingexcel/ResolvingExcelApplication.java
@@ -0,0 +1,12 @@
+package com.tqc.resolvingexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolvingExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolvingExcelApplication.class, args);
+	}
+}
